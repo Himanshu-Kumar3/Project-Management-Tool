fd4f96a248299d316dffe69130fdb9fc37b18d36
Himanshu Kumar <himansu9508@gmail.com>
2026-04-08 16:47:37 +0530

    added trim user

--- Server/src/Routes/user.js
+++ Server/src/Routes/user.js
@@ -17,13 +17,31 @@ userRouter.post("/user/createWorkspace" ,userAuth , async(req, res)=>{
                   slug,
                   ownerId : _id
             })
-
-            const data =await  workspace.save();
-            res.json({message:"Workspace created successfuly" , data})
+            const  isWorkspace = await Workspace.findOne({ownerId : _id , name : name})
+            if(!isWorkspace){
+                   const data =await  workspace.save();
+                   res.json({message:"Workspace created successfuly " + name , data})       
+            }else{
+                 throw new Error("User Already Exists")
+            }
+           
       }catch(er){
             res.status(400).json({message : "Error :"  + er.message})
       }
 });
+
+userRouter.get('/user/getWorkspace' , userAuth , async (req, res)=>{
+      try{
+            const user = req.user;
+            const workspaces = await Workspace.find({ownerId : user._id});
+            if(workspaces.length === 0){
+                 return  res.status(404).json({message : "No workspaces found , Please Create Workspace"})
+            }
+            res.json({message :"Workspaces ...!" , data : workspaces} );
+      }catch(er){
+            res.json("ERROR : " + er.message);
+      }
+})
 
 
 module.exports = userRouter;
