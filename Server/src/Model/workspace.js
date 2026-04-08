e5d597bb66c9a9dcd84b45a9b6776e18d7cdf4e6
Himanshu Kumar <himansu9508@gmail.com>
2026-04-08 16:24:27 +0530

    workspace model

---
+++ Server/src/Model/workspace.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const workspaceSchema = new mongoose.Schema({
+      name : {
+            type:String,
+            required:true,
+            maxlength:20,
+            minlength:4
+      },
+      slug:{
+            type:String,
+            required:true,
+            maxlength:20,
+            minlength:4
+      },
+      photoUrl:{
+            type:String,
+      },
+      discription:{
+            type:String,
+            default:"This is a default discription"
+      },
+      ownerId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref :"User",
+            required:true
+      }
+      , members : [{
+            userId :{
+                  type:mongoose.Schema.Types.ObjectId,
+            },
+            workspaceId :{
+                  type:mongoose.Schema.Types.ObjectId
+            },
+            message:{
+                  type:String
+            },
+            role:{
+                  type:String,
+                  enum:{
+                        values :["Admin" , "Member"],
+                        message : '{VALUE} is not valid role'
+                  }
+            }
+      }]
+}, {timestamps:true});
+
+Workspace = mongoose.model("Workspace" , workspaceSchema);
+
+module.exports = Workspace;
