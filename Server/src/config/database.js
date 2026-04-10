ea5f5eb0b6164308d072aa68b935fc9cac82f516
Himanshu Kumar <himansu9508@gmail.com>
2026-04-10 14:02:21 +0530

    connects with mongoose atlas server

--- Server/src/config/database.js
+++ Server/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
-        await mongoose.connect('mongodb://localhost:27017/Project-Management-Tool');
+        await mongoose.connect('mongodb://himanshu23:Ut5vvyqdWvSt4Qnq@ac-7mngqsu-shard-00-00.4hxwqag.mongodb.net:27017,ac-7mngqsu-shard-00-01.4hxwqag.mongodb.net:27017,ac-7mngqsu-shard-00-02.4hxwqag.mongodb.net:27017/Project-Management?ssl=true&replicaSet=atlas-10jvjz-shard-0&authSource=admin&appName=DevTinder/');
 }
 
 module.exports = connectDB;
