8be273445a37753e1f0bd280df6790c361163427
Himanshu Kumar <himansu9508@gmail.com>
2026-04-25 09:49:19 +0530

    added task actions and reducers

--- client/src/utils/appstore.js
+++ client/src/utils/appstore.js
@@ -4,13 +4,15 @@ import userReducer from "./userSlice";
 import workspaceReducer from "./workspaceSlice";
 import themeReducer from "./themeSlice";
 import projectReducer from "./projectSlice";
+import taskReducer from "./taskSlice";
 
 const store = configureStore({
       reducer:{
             user : userReducer,
             workspace : workspaceReducer,
             project : projectReducer,
-            theme : themeReducer
+            theme : themeReducer, 
+            task: taskReducer
       }
 
 })
--- client/src/utils/taskSlice.js
+++ client/src/utils/taskSlice.js
@@ -1,6 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const taskSlice = createSlice({
-      name : "task" , 
+      name : "task" ,
+      initialState:{
+            task: null, 
+            tasks:[]
+      } ,
+      reducers:{
+            addTask(state , action){
+                  state.task = action.payload
+            }, appendTask(state , action){
+                  state.tasks.push(action.payload);
+            }, addTasks(state , action){
+                  state.tasks = action.payload;
+            },removeTask(state , action){
+                  state.tasks.filter(task => !task.includes(action.payload))
+            }
+      }
       
-})
+});
+
+export const {addTask, addTasks , appendTask , removeTask} = taskSlice.actions;
+export default taskSlice.reducer;
