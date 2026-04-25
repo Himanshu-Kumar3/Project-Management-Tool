1e6930d994357eda8ad8739fbb641edf4aa1618e
Himanshu Kumar <himansu9508@gmail.com>
2026-04-25 09:48:44 +0530

    added projects config

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,25 +1,54 @@
-import React, { useState } from 'react'
-import {useSelector} from "react-redux";
+import React, { useEffect, useState } from 'react'
+import {useDispatch, useSelector} from "react-redux";
 import CreateProject from './CreateProject';
 import ProjectCard from './ProjectCard';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
+import { addprojects } from '../utils/projectSlice';
+import { Link } from 'react-router-dom';
 
 const Projects = () => {
   const {projects} = useSelector(store=> store.project);
   const {workspace} = useSelector(store => store.workspace); 
 
+  const dispatch = useDispatch();
   const [isCreateProject , setIsCreateProject] = useState(false);
   const handleCreateProject = ()=>{
     setIsCreateProject(true);
   }
+    
+ const getProjects = async()=>{
+            try{
+                  if(projects.length !== 0) return ;
+                  const res =  await axios.post(BASE_URL + "/workspace/getProjects/" +workspace._id  , {} ,{withCredentials:true} );
+
+                  
+                  dispatch(addprojects(res?.data?.data));
+
+            }catch(er){
+                  console.log(er.message);
+            }
+      }
+
+
+
+
+  useEffect(()=>{
+    if(!projects || projects.length===0){
+      getProjects();
+    }
+
+  } ,[]);    
 
   if(!projects) return;
-  console.log(projects)
+ 
 
-  const recentProject = [];
 
+
+ console.log(isCreateProject)
 
   return (
-    <div className='w-full h-[100%] p-6 px-10 overflow-hidden'>
+    <div className='w-full h-[100%] p-6 px-10  scrollbar-hide'>
       <div className='flex w-full justify-between items-center px-1 '>
         <div className='title'>
            <h2 className='font-bold text-2xl'>
@@ -27,7 +56,7 @@ const Projects = () => {
            </h2>
            <p className='text-sm text-zinc-600' >Manage and track your projects</p>
         </div>
-        <button className=' text-sm h-8 px-2 text-white rounded-xs shadow-sm cursor-pointer bg-blue-500 pr-3'><i className="fa-solid fa-plus"></i> New Task</button>
+        <button type='button' className='text-sm h-8 px-2 text-white rounded-xs shadow-sm cursor-pointer bg-blue-500 pr-3' onClick={handleCreateProject} ><i className="fa-solid fa-plus"></i> New Project</button>
       </div>
       <div className='mt-6 flex items-baseline'>
         <div className='text-sm border border-gray-400 w-68 rounded-sm  focus-within:ring-1 py-1 mt-2'>
@@ -66,8 +95,14 @@ const Projects = () => {
           ))}
         </div>
         
+        
+        <div>
+          
        {isCreateProject && <CreateProject data={workspace} onClose={()=>setIsCreateProject(false)}/>}
 
+
+        </div>
+        
     </div>
   )
 }
