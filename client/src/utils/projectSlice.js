7f83fa44811f69d261364a00d3d64e0a6bb6f080
Himanshu Kumar <himansu9508@gmail.com>
2026-05-03 06:57:55 +0530

    added updateProject

--- client/src/utils/projectSlice.js
+++ client/src/utils/projectSlice.js
@@ -16,10 +16,21 @@ const projectSlice = createSlice({
             appendProject(state , action){
                   state.projects.push(action.payload);
             },
+            updateProjects(state , action){
+                  const {_id , ...updatedData} = action.payload;
+                  const index = state.projects.findIndex(project => project._id === _id);
+                  if(index !== -1){
+                        state.projects[index] = {...state.projects[index] , ...updatedData}
+                  }
+                  // Also update current project if it's the same
+                 if (state.project?._id === _id) {
+                    state.project = { ...state.project, ...updatedData };
+                 }
+            },
             removeProject(state , action){
-                  state.projects.filter((item)=> item !== action.payload);
+                  state.project = state.projects.filter((item)=> item !== action.payload);
             }
       }
 });
-export const {addProject , addprojects , appendProject, removeProject} = projectSlice.actions;
+export const {addProject , addprojects , appendProject,updateProjects, removeProject} = projectSlice.actions;
 export default projectSlice.reducer;
