a81e63e8770eaafd0b44e77f239b69b90d56a506
Himanshu Kumar <himansu9508@gmail.com>
2026-05-04 03:13:02 +0530

    deleted unneccessary files

--- client/src/components/WelcomeBody.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WelcomeBody = () => {
-  return (
-    <div>WelcomeBody</div>
-  )
-}
-
-export default WelcomeBody;
--- client/src/components/WelcomeFooter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WelcomeFooter = () => {
-  return (
-    <div>WelcomeFooter</div>
-  )
-}
-
-export default WelcomeFooter;
