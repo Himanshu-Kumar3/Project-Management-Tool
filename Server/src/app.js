6dcfed3f7ddb8a643b13bb2c6b8f4433c311a107
Himanshu Kumar <himansu9508@gmail.com>
2026-05-04 13:45:00 +0530

    updated cors

--- Server/src/app.js
+++ Server/src/app.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-      origin:"http://localhost:5173",
+      origin:"https://frontend-project-management-chi.vercel.app/",
       credentials:true,
       methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
 }));
